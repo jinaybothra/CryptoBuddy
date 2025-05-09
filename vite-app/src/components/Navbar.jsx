e26b5da747d84a0dfc8be7dc335cc7969fd7a4a1
jinaybothra <jinaydeep2001@gmail.com>
2025-05-09 21:04:48 +0530

    finalized changes

--- vite-app/src/components/Navbar.jsx
+++ vite-app/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
       <div className="logo-container">
         <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
-          <Link to="/">CoinTracker</Link>
+          <Link to="/">CryptoBuddy</Link>
         </Typography.Title>
         <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
           <MenuOutlined/>
